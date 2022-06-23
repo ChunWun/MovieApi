2040158f411d04fd6be8c2f499b118c6f2a7e171
ChunWun <94582200+ChunWun@users.noreply.github.com>
2022-06-23 18:32:58 +0800

    add error

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import React, { useState } from 'react';
 import MoviesList from './components/MoviesList';
 import './App.css';
 
-function App() {
+const MOVIE_API_URL = 'https://swapi.dev/api/films/';
+const LOADING = 'Loading...';
+const NO_MOVIES = 'Found no movies.';
+
+const App = () => {
   const [movies, setMovies] = useState([])
+  const [pageState, setPageState] = useState();
 
   const fetchMoviesHandler = async () => {
-    fetch('https://swapi.dev/api/films/')
-      .then((response) => {
-        return response.json();
+
+    try {
+      const response = await fetch(MOVIE_API_URL);
+      setPageState(() => {
+        return <p>{LOADING}</p >
       })
-      .then((data) => {
+      if (!response.ok) {
+        throw new Error('Something went wrong');
+      } else {
+        const data = await response.json();
         const moviesData = data.results.map((movieData) => {
           return {
             id: movieData.episode_id,
@@ -22,7 +32,13 @@ function App() {
         })
 
         setMovies(moviesData);
-      })
+        setPageState(() => {
+          return moviesData.length > 0 ? <MoviesList movies={moviesData} /> : <p>{NO_MOVIES}</p>;
+        })
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
   }
 
   return (
@@ -31,7 +47,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
-        <MoviesList movies={movies} />
+        {pageState}
       </section>
     </React.Fragment>
   );
